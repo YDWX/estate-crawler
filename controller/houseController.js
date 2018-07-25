90dda239e5a8f63eed57a470b66ea1b849b5249c
YDWX <h979985773@gmail.com>
2018-07-25 17:35:36 +0800

    检查房产是否已存在

--- controller/houseController.js
+++ controller/houseController.js
@@ -2,6 +2,10 @@ const model = require('../models/index')
 
 module.exports = {
   create(houseData) {
+    const {houseId, agency} = houseData
+    model.house.find({houseId, agency}).exec().then((result)=>{
+      
+    })
     model.house.create(houseData).then((result) => {
 
     })
